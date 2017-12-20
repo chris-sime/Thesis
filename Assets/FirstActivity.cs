4a3128158d09f5176244f5fdfe3bc5d66ae8e01b
Hrystos <krisimen@gmail.com>
2017-12-20 18:43:57 +0200

    Prototyping
    Very early stages. Trying to get around Vuforia and android develpment

---
+++ Assets/FirstActivity.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirstActivity : MonoBehaviour {
+
+    bool PhiliposCompleted = false;
+    bool FalaggaCompleted = false;
+    bool HeroniaCompleted = false;
+
+    public void SetPhiliposCompleted() { PhiliposCompleted = true; }
+    public void SetFalagaCompleted() { FalaggaCompleted = true; }
+    public void SetHeroniaCompleted() { HeroniaCompleted = true; }
+
+    public GameObject InfoPanel;
+    public GameObject[] GameObjects;
+
+    public void OnClickInfoPanelButton()
+    {
+        InfoPanel.SetActive(false);
+        if (!PhiliposCompleted)
+        {
+            SetPhiliposCompleted();
+        }
+        else if (!FalaggaCompleted)
+        {
+            SetFalagaCompleted();
+        }
+        else
+        {
+            SetHeroniaCompleted();
+        }
+    }
+
+    private void Update()
+    {
+        if (PhiliposCompleted)
+        {
+            GameObjects[0].SetActive(false);
+            GameObjects[1].SetActive(true);
+            GameObjects[2].SetActive(true);
+            GameObjects[3].SetActive(true);
+            //GameObject.Find("").SetActive(true);
+            //GameObject.Find("").SetActive(true);
+            //GameObject.Find("").SetActive(true);
+            //GameObject.Find("").SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectInfo.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectInfo : MonoBehaviour {
+
+    public string _name;
+    public string _info;
+
+    public GameObject InfoPanel;
+
+    Camera cam;  
+    float countdownTime = 2f;
+
+	// Use this for initialization
+	void Start () {
+        
+        cam = FindObjectOfType<Camera>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Transform objectHit;       
+        Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width/2f + 20, Screen.height/2, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            objectHit = hit.transform;
+            if(objectHit == this.transform)
+            {
+                countdownTime -= Time.deltaTime;
+                if(countdownTime < 0)
+                {
+                    Debug.Log(objectHit.name);
+                    InfoPanel.GetComponentsInChildren<Text>()[0].text = _name;
+                    InfoPanel.GetComponentsInChildren<Text>()[1].text = _info;
+                    InfoPanel.SetActive(true);
+                }
+
+            }
+        }
+        else
+        {
+            countdownTime = 2f;
+            InfoPanel.SetActive(false);
+        }
+        
+    }
+}
