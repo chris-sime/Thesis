e4d85c11e2a5baf55b7e598fe263f0e8c65cd55f
Hrystos <krisimen@gmail.com>
2018-03-26 13:59:47 +0300

    Activity 3 Prototyping
    +Implement Quiz-Like minigame
    +Imported new graphic assets

---
+++ Assets/QuestionQuiz.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class QuestionQuiz : MonoBehaviour {
+
+    [SerializeField] Text QuestionPanelText;
+    [Space]
+    [SerializeField]
+    [TextArea(2,10)]
+    private string question;
+
+	// Use this for initialization
+	void Start () {
+        QuestionPanelText.text = question;
+	}
+	
+    public void CorrectAnswer()
+    {
+
+    }
+}
